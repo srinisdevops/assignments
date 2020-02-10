381e2404d86592692bb532e00caf1db6cad644aa
Ubuntu <ubuntu@ip-172-31-16-229.us-east-2.compute.internal>
2020-02-10 05:08:46 +0000

    assignment useradd

--- useradd_script.sh
+++ useradd_script.sh
@@ -1,1 +1,11 @@
-#
+#!/bin/bash
+# Read each line of file into variable line
+while IFS= read -r line
+# Continue until all the lines read
+do
+# cut the first word from lune and then creae username
+	sudo useradd -m $(echo $line | cut -d " " -f1)
+# reading from file data.txt
+done < "data.txt"
+
+
