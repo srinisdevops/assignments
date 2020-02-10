ea11d82c470ae0dc25eb57e17c60295d8fea92d4
Ubuntu <ubuntu@ip-172-31-16-229.us-east-2.compute.internal>
2020-02-10 05:35:55 +0000

    added password

--- useradd_script.sh
+++ useradd_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Read each line of file into variable line
+# Read each line of the text file, "data.txt", into a variable "line"
 while IFS= read -r line
 # Continue until all the lines read
 do
-# cut the first word from lune and then creae username
-	sudo useradd -m $(echo $line | cut -d " " -f1)
+# cut the first word from line for username and second word for password
+	sudo useradd -m $(echo $line | cut -d " " -f1) -p $(echo $line | cut -d " " -f2)
 # reading from file data.txt
 done < "data.txt"
 
