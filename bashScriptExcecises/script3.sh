ef20071679cdba8a00de5262afafda548e5b865a
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 14:45:39 -0700

    added comments

--- bashScriptExcecises/script3.sh
+++ bashScriptExcecises/script3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# The program takes an argument and compares it with 0 using conditional statements and operations tested were gt for greater than  
+# The program takes an argument and compares it with 0 using IF conditional statements
+# gt, ge, lt, le, ne, eq are tested
+# if we execute this with the following command "./script3.sh 5" then it will print 5>0, 5>=0, 5!=0
+
+
+
 if test $1 -gt 0
 then
         echo "$1 > 0"
