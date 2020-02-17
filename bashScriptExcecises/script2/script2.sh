107ba9e7e86edcc31f6fa1ab5003169f92befadf
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 16:01:29 -0700

    comments added

--- bashScriptExcecises/script2/script2.sh
+++ bashScriptExcecises/script2/script2.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 who | sort
+# prints logged in users in sorting order
+
 who | sort > user_list
+# creates file "user_list" with sorted logged in users
+
 who | wc -l
+# prints number of logged in users
+
 ls -l | wc -l
+#prints number of files and folders in the present folder
+
 who | grep cZ
+# checks if there is any logged in user with "cZ" in their names
