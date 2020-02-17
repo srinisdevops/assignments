5e4f21728b5b91137718a020e2b96b996d68613b
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 15:46:32 -0700

    comments added

--- bashScriptExcecises/script8.sh
+++ bashScriptExcecises/script8.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Takes first argument and prints multiplication table up to 10. If there are no arguments, prints error message and exits out of program.
+
 if [ $# -eq 0 ]
 then
 echo "Error - Number missing form command line argument"
