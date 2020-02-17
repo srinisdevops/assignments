d31cb3fdffe6d900507e18843b04c2ac7cc1d2cf
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 15:55:14 -0700

    comments added

--- bashScriptExcecises/script1.bash
+++ bashScriptExcecises/script1.bash
@@ -1,9 +1,17 @@
 #!/bin/bash
+# echo statements will be printed
+
 echo "ls > file_list"
 ls > file_list
- 
+
+# creates file "file_list" with ls command output
+
 echo "ls -la >> file_list"
 ls -la >> file_list
- 
+
+# Appends file "file_list" with ls -la command output
+
 echo "cat < file_list"
 cat < file_list
+
+# prints contents of file "file_list"
