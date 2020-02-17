f148534e870d4a48a64fbbaffda155ef1fea9387
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 15:29:38 -0700

    comments added

--- bashScriptExcecises/script7.sh
+++ bashScriptExcecises/script7.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# The code takes an argument and checks whether it is greater than 0, less than 0, equal to 0. otherwise it will print
+# the message "oops! argument is not number, give number"
+
 if [ $1 -gt 0 ]; then
   echo "$1 is positive"
 elif [ $1 -lt 0 ]
