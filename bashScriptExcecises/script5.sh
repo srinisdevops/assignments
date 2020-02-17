d238e303833067da145e07f94987e46b42350129
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 14:29:16 -0700

    Update script5.sh

--- bashScriptExcecises/script5.sh
+++ bashScriptExcecises/script5.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 string_null=""
 string1="string1"
- 
+
+# "STRING -z" command evaluates to "TRUE" when the STRING is null
+# "STRING -n" command evaluates to "FALSE" when the STRING is null
+
+
 if [ $string_null -n ]
 then
         echo "not null string"
 else
         echo "null string"
 fi
- 
+
+# Output is "null string" as the $string_null -n evaluates to "FALSE" when the $string_null is null.
+
 if [ $string_null -z ]
 then
         echo "null string"
 else
         echo "not null string"
 fi
- 
+
+# Output is "null string" as the $string_null -z evaluates to "TRUE" when the $string_null is null.
+
 if [ "$string_null" == "$string1" ]
 then
         echo "strings equal"
 else
         echo "strings not equal"
 fi
- 
+
+# Output is "strings not equal as the two strings are not equal. First string is null second string is String1.
+
 if [ "$string_null" != "$string1" ]
 then
         echo "strings not equal"
 else
         echo "strings equal"
 fi
+
+# Output is "strings not equal as the two strings are not equal. First string is null second string is String1.
