f2f0dfe97fd06ef3209ee228250c4abfb0899f5d
srinisdevops <60857554+srinisdevops@users.noreply.github.com>
2020-02-17 14:43:28 -0700

    added comments

--- bashScriptExcecises/script4.sh
+++ bashScriptExcecises/script4.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This script takes an argument and compares with 0 using comparison operators lt (Less than) and le (Less than or equal to)
+# using IF conditional statements
+# If we execute this script using "script4.sh -4" then it will print -4<0 and -4<=0.
+
 if test $1 -lt 0
 then
         echo "$1 < 0"
